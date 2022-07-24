24102e73a115270adcf99b0a8da00fb5b312c965
Vic Yang <yanggf008@126.com>
2022-07-25 10:38:44 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,3 +1,7 @@
 import streamlit
 
 streamlit.title('My parents New Healthy Dinner')
+streamlit.header('Breakfast Menue')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
