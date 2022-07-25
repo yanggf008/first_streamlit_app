0b832f44354ed5f8b5c8c0cfb8d9696cc2930509
Vic Yang <yanggf008@126.com>
2022-07-25 16:28:19 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,6 @@
 import streamlit
+import snowflake.connector
+from urllib.error import URLError
 
 streamlit.title('My parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
@@ -27,19 +29,23 @@ import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
 streamlit.text(fruityvice_response.json())
 streamlit.header("Fruityvice Fruit Advice!")
-
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+try:
+  fruit_choice = streamlit.text_input('What fruit would you like information about?')
+  if not fruit_choice:
+    streamlit.error("please select a fruit to get information.")
+  else:
+    fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
+    fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+    streamlit.dataframe(fruityvice_normalized)
 
 # write your own comment -what does the next line do? 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+
 # write your own comment - what does this do?
-streamlit.dataframe(fruityvice_normalized)
+
 
 
 streamlit.stop()
-import snowflake.connector
-from urllib.error import URLError
+
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
