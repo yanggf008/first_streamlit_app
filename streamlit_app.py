205ed9cd661014e6ac41ad99ecb3ea196e26062a
Vic Yang <yanggf008@126.com>
2022-07-25 16:24:19 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,9 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
+sreamlit.stop()
 import snowflake.connector
+from urllib.error import URLError
 
 
 my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
