1fb6b2075680c94132773a341f880ea9579fa302
Vic Yang <yanggf008@126.com>
2022-07-25 16:28:46 +1200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -37,7 +37,8 @@ try:
     fruityvice_response = requests.get("https://fruityvice.com/api/fruit/" + fruit_choice)
     fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
     streamlit.dataframe(fruityvice_normalized)
-
+except URLError as e:
+  streamlit.error()
 # write your own comment -what does the next line do? 
 
 # write your own comment - what does this do?
